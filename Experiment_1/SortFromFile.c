1004ebf138dc07d3475e5b2fb20129e86afbe992
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-01-06 22:20:12 +0530

    Perfomances Calculation added

--- Experiment_1/SortFromFile.c
+++ Experiment_1/SortFromFile.c
@@ -71,6 +71,10 @@ void writeIntoFile(char* fileName,Student studentArray[],int numberOfStudents){
     fclose(sortedData);
 }
 
+double calculateCpuSpeed(clock_t start,clock_t end){
+    return ((double) end-start)/CLOCKS_PER_SEC;
+}
+
 int main(){
     FILE *studentData=fopen("StudentData.txt","r");
 
@@ -82,6 +86,10 @@ int main(){
     Student students[MAX_STUDENT_NUMBER];
     Student copyOfStudents[MAX_STUDENT_NUMBER];
     int numberOfStudents=0;
+    clock_t bubble_sort_start,bubble_sort_end;
+    clock_t quick_sort_roll_start,quick_sort_roll_end;
+    clock_t quick_sort_name_start,quick_sort_name_end;
+    clock_t quick_sort_marks_start,quick_sort_marks_end;
 
     while(fscanf(studentData,"%d,%[^,],%f",&students[numberOfStudents].rollNumber,students[numberOfStudents].name,&students[numberOfStudents].totalMarks)!=EOF){
         numberOfStudents++;
@@ -92,32 +100,51 @@ int main(){
     printf("The Student Data:\n");
     printStudentArray(students,numberOfStudents);
 
+    //Bubble Sort By Roll
     copyArray(students,copyOfStudents,numberOfStudents);
+    bubble_sort_start=clock();
     bubbleSort(copyOfStudents,numberOfStudents);
+    bubble_sort_end=clock();
 
     printf("The Student Data after sorting [By roll, bubblesort]:\n");
     printStudentArray(copyOfStudents,numberOfStudents);
     writeIntoFile("BubbleSort.txt",copyOfStudents,numberOfStudents);
 
+    //Quick Sort By Roll
     copyArray(students,copyOfStudents,numberOfStudents);
+    quick_sort_roll_start=clock();
     qsort(copyOfStudents,numberOfStudents,sizeof(Student),compareByRollNumber);
+    quick_sort_roll_end=clock();
 
     printf("The Student Data after sorting [By roll, qsort]:\n");
     printStudentArray(copyOfStudents,numberOfStudents);
     writeIntoFile("QuickSortRoll.txt",copyOfStudents,numberOfStudents);
 
+    //Quick Sort By Marks
     copyArray(students,copyOfStudents,numberOfStudents);
+    quick_sort_marks_start=clock();
     qsort(copyOfStudents,numberOfStudents,sizeof(Student),compareByTotalMarks);
+    quick_sort_marks_end=clock();
 
     printf("The Student Data after sorting [By total marks, qsort]:\n");
     printStudentArray(copyOfStudents,numberOfStudents);
     writeIntoFile("QuickSortMarks.txt",copyOfStudents,numberOfStudents);
 
+    //Quick Sort By Names
     copyArray(students,copyOfStudents,numberOfStudents);
+    quick_sort_name_start=clock();
     qsort(copyOfStudents,numberOfStudents,sizeof(Student),compareByName);
+    quick_sort_name_end=clock();
 
     printf("The Student Data after sorting [By name, qsort]:\n");
     printStudentArray(copyOfStudents,numberOfStudents);
     writeIntoFile("QuickSortNames.txt",copyOfStudents,numberOfStudents);
+
+    //Perfomances
+    printf("PERFORMANCES OF THE SORTING METHODS\n");
+    printf("Bubble sort: %f s\n", calculateCpuSpeed(bubble_sort_start,bubble_sort_end));
+    printf("qsort (by roll no.): %f s\n", calculateCpuSpeed(quick_sort_roll_start,quick_sort_roll_end));
+    printf("qsort (by name): %f s\n", calculateCpuSpeed(quick_sort_name_start,quick_sort_name_end));
+    printf("qsort (by marks): %f s\n", calculateCpuSpeed(quick_sort_marks_start,quick_sort_marks_end));
     return 0;
 }
